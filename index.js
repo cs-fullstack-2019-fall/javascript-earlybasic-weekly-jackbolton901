639bbd503999632886d45e77f98a2b3b4e5c8432
jackbolton901 <bltnjck@gmail.com>
2019-08-09 15:56:40 -0500

    Weekly Project

---
+++ index.js
@@ -0,0 +1,36 @@
+//index.js
+q= false
+var opt1 = prompt("Enter 1 to count, 2 for calculator, or q to quit.");
+if(opt1 === 1)
+{
+var num =1;
+var opt2 = parseInt(prompt("To count by 1s, enter the number you want to count to."));
+while(num <= opt2)
+{
+	console.log(num);
+	num++;
+}
+}
+else if (opt1===2) 
+{
+	var a=prompt("Press a to add");
+	if (a ==="a")
+	{
+		var num1= parseInt(prompt("Enter number."));
+		var num2= parseInt(prompt("Enter number."));
+		alert(num1 + num2);
+	}
+
+}
+
+var num =1;
+var yes= prompt("Would you like to count by 2s? If so, type yes.");
+ if(yes === "yes")
+ {
+var twos=(parseInt(prompt("Enter number."));
+while(num<= twos)
+  {
+   console.log(num);
+  }
+ num=(num+2);
+}
